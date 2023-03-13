5c335a77c8547a17d60ef44682affad53fb09484
SumitKumar-2000 <sumitkumarracer@gmail.com>
2023-03-14 03:05:33 +0530

    [App.jsx] backend route added and all errors checked

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,32 @@
+import { useState, useEffect } from "react";
+import axios from "axios"
+
 function App() {
+  const base_url = import.meta.env.BACKEND_URL;  
+  const [apiResponse, setApiResponse] = useState({})
+  const [filterGenre, setFilterGenre] = useState([])
+  const [page, setPage] = useState(1)
+  const [search, setSearch] = useState("")
+  const [limit, setLimit] = useState(5)
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const {data} = await axios.get("http://localhost:8080/api/movies", {
+          params : {
+            page : page,
+            genre : filterGenre.join(","),
+            search : search,
+            limit : limit
+          }
+        })
+          
+        console.log("response: ", data);
+      } catch (error) {
+        console.log("error: ",error);
+      }
+    })()
+  },[])
 
   return (
     <div>
