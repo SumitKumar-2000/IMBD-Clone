9bb682245228135c1b4e829674805a6729e18031
SumitKumar-2000 <sumitkumarracer@gmail.com>
2023-03-16 00:49:51 +0530

    [client side] final ui updated

--- client/src/components/listViewSection/index.jsx
+++ client/src/components/listViewSection/index.jsx
@@ -9,7 +9,7 @@ const ListViewSection = ({apiResponse}) => {
           apiResponse.movies?.map((movie,idx) => {
             return (
               <div key={idx} className='w-full rounded-sm shadow-sm border border-slate-300 bg-white mb-1 flex items-center justify-between p-2'>
-                <div>
+                <div className='flex items-center md:w-[400px] w-[150px]'>
                   <div className='h-[100px] w-[80px]'>
                     <img 
                       src={movie.img} 
@@ -17,15 +17,23 @@ const ListViewSection = ({apiResponse}) => {
                       className='h-full w-full object-cover'
                     />
                   </div>
-                  <div>
-                    
+                  <div className='ml-2 md:text-lg text-xs font-medium'>
+                    {movie.name}, {movie.year}
                   </div>
                 </div>
-                <div>
-                  
+                <div className='w-[50px] md:w-[200px]'>
+                  {
+                    movie.genre.map((gen,idx) => {
+                      return <span key={idx} className='font-medium md:text-lg text-xs text-black'>
+                        {gen}
+                        {idx !== movie.genre.length -1 && " / "}
+                      </span>
+                    })
+                  }
                 </div>
-                <div>
-                  
+                <div className='flex items-center gap-2'>
+                  <span className='md:text-2xl text-yellow-400'>&#9733;</span>
+                  <span className='md:text-lg text-xs font-medium'>{movie.rating}</span>
                 </div>
               </div>
             )
