504e5b9831236ac442f148d8ae9e4abc30e7d77f
SumitKumar-2000 <sumitkumarracer@gmail.com>
2023-03-16 00:58:00 +0530

    [paginationSection.jsx] page active state added

--- client/src/components/paginationSection/index.jsx
+++ client/src/components/paginationSection/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PaginationSection = ({apiResponse, setPage}) => {
-
+  
+  const [activePage, setActivePage] = useState(1);
   const {total,limit} = apiResponse;
   const pageCount = Math.ceil(total/limit);
   let arr = [];
@@ -16,8 +17,8 @@ const PaginationSection = ({apiResponse, setPage}) => {
         arr.map(page => {
           return <button 
                   key={page} 
-                  className="rounded-[50%] px-3 py-1 border-2 border-black hover:bg-yellow-400 focus:bg-yellow-400 font-bold md:text-base"
-                  onClick={()=>setPage(page)}
+                  className={`${activePage === page && "bg-yellow-400"} rounded-[50%] px-3 py-1 border-2 border-black hover:bg-yellow-400 font-bold md:text-base`}
+                  onClick={()=>{setPage(page), setActivePage(page)}}
                  >
                   {page}              
                  </button>
